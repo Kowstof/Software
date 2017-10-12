20ebd14ce218a5414825f5a3de0abbc6c0f91a32
Krystof Pavlis <Krystof.Pavlis@education.nsw.gov.au>
2017-10-12 12:14:35 +1100

    Add a scret ;)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HelloWorld
@@ -15,7 +16,19 @@ namespace HelloWorld
             Console.WriteLine("Oh no! The program failed! \nTry to override the ADP array, maybe it will back\nup the redundant microchip!");
             Console.ReadLine();
             Console.ReadLine();
+            Console.Beep(659, 125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(523, 125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(375);
+            Console.Beep(392, 125);
             Console.WriteLine("You found a scret ;)");
+            Console.ReadLine();
         }
     }
 }
