5b9987eab211efb9aed3ed887f1dfd2932bd80ac
Krystof Pavlis <Krystof.Pavlis@education.nsw.gov.au>
2017-10-12 12:39:55 +1100

    Create WelcomeMessage program

---
+++ WelcomeMessage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User: ");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Authenticating...");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine($"User recognized. Welcome, {name}");
+            Console.ReadKey();
+        }
+    }
+}
