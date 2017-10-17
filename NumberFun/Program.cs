fafe2c4dae893bcb66460af1f853aa89a9299579
Krystof Pavlis <Krystof.Pavlis@education.nsw.gov.au>
2017-10-18 09:58:26 +1100

    Create NumberFun program

---
+++ NumberFun/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("| Welcome to the NUMBER MASHER! |");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+            Console.WriteLine("Please enter X:");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Y:");
+            float y = float.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine($"{x} + {y} = {(x + y)}");
+            Console.WriteLine($"{x} - {y} = {(x - y)}");
+            Console.WriteLine($"{x} × {y} = {(x * y)}");
+            Console.WriteLine($"{x} ÷ {y} = {(x / y)}");
+
+            Console.ReadLine();
+        }
+    }
+}
