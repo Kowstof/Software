c7da4ac8225860008b0fe7b8a3727cbfe85a9ec6
Krystof Pavlis <Krystof.Pavlis@education.nsw.gov.au>
2017-10-18 10:54:35 +1100

    Make the door "animation" a function

--- PickADoor/Program.cs
+++ PickADoor/Program.cs
@@ -16,81 +16,25 @@ namespace PickADoor
 
             if (choice == "1")
             {
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine("     /|");
-                Console.WriteLine("    / |");
-                Console.WriteLine("   /__|______");
-                Console.WriteLine("  |  __  __  |");
-                Console.WriteLine("  | |  ||  | | ");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |  __  __()|   ...opening...");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |__________|");
-                Thread.Sleep(2000);
-                Console.Clear();
+                ShowDoor();
                 Console.WriteLine("Behind door 1 is the kitchen");
                 Console.ReadKey();
             }
             else if (choice == "2")
             {
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine("     /|");
-                Console.WriteLine("    / |");
-                Console.WriteLine("   /__|______");
-                Console.WriteLine("  |  __  __  |");
-                Console.WriteLine("  | |  ||  | | ");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |  __  __()|   ...opening...");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |__________|");
-                Thread.Sleep(2000);
-                Console.Clear();
+                ShowDoor();
                 Console.WriteLine("Behind door 2 is the entrance to the dungeon");
                 Console.ReadKey();
             }
             else if (choice == "3")
             {
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine("     /|");
-                Console.WriteLine("    / |");
-                Console.WriteLine("   /__|______");
-                Console.WriteLine("  |  __  __  |");
-                Console.WriteLine("  | |  ||  | | ");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |  __  __()|   ...opening...");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |__________|");
-                Thread.Sleep(2000);
-                Console.Clear();
+                ShowDoor();
                 Console.WriteLine("Behind door 3 is a kid insulting your mum on Call of Duty");
                 Console.ReadKey();
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine("     /|");
-                Console.WriteLine("    / |");
-                Console.WriteLine("   /__|______");
-                Console.WriteLine("  |  __  __  |");
-                Console.WriteLine("  | |  ||  | | ");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |  __  __()|   ...opening...");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |  ||  | |");
-                Console.WriteLine("  | |__||__| |");
-                Console.WriteLine("  |__________|");
-                Thread.Sleep(2000);
-                Console.Clear();
+                ShowDoor();
                 Console.WriteLine($"You concentrated so hard to choose a door that doesn't exist, that you created a portal to another dimension with your mind, and teleported yourself there, so that you could choose door {choice}. This also disprove all of our known laws of physics, therefore breaking our universe.");
                 Console.WriteLine();
                 Console.WriteLine("Press enter to continue...");
@@ -112,6 +56,25 @@ namespace PickADoor
                 Console.WriteLine($"I hope whatever was behind door {choice} was worth it...");
                 Console.ReadKey();
             }
+        }
+
+        private static void ShowDoor()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /__|______");
+            Console.WriteLine("  |  __  __  |");
+            Console.WriteLine("  | |  ||  | | ");
+            Console.WriteLine("  | |__||__| |");
+            Console.WriteLine("  |  __  __()|   ...opening...");
+            Console.WriteLine("  | |  ||  | |");
+            Console.WriteLine("  | |  ||  | |");
+            Console.WriteLine("  | |__||__| |");
+            Console.WriteLine("  |__________|");
+            Thread.Sleep(2000);
+            Console.Clear();
         }
     }
 }
