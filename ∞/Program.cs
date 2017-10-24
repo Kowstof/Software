852577d001f73b22047885e134f6dc9f154216cf
Krystof Pavlis <Krystof.Pavlis@education.nsw.gov.au>
2017-10-24 14:32:51 +1100

    Create index system. Make loop program

---
+++ ∞/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to count with me!");
+            Console.ReadLine();
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
